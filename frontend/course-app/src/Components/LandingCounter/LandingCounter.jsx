98474682fd44e83cec75f7a049e832cf0e8df6a2
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-07 12:59:55 +0330

    The landingCount component is added to handle a number with the submitted value.

---
+++ frontend/course-app/src/Components/LandingCounter/LandingCounter.jsx
@@ -0,0 +1,15 @@
+import React,{useEffect,useState} from 'react'
+
+export default function LandingCounter({count}) {
+
+    const [courseCounter,setCouresCounter ]=useState(0)
+    useEffect(()=>{let interval = setInterval(()=>{setCouresCounter(prevCount => prevCount + 1)},1);
+    if (courseCounter === count){
+      clearInterval(interval)
+    }
+    return()=>clearInterval(interval)
+  },[courseCounter])
+  return (
+    <span class="landing-status__count">{courseCounter}</span>
+  )
+}
