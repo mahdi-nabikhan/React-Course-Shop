c4920d6570e3cc473aa40450f173db2bbcfa1848
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-07 13:29:51 +0330

    The ArticleBox component was added to display the contents of an article. The LastArticle component also had some changes, such as the addition of an Article Box to it.

--- frontend/course-app/src/Components/LastArticles/LastArticles.jsx
+++ frontend/course-app/src/Components/LastArticles/LastArticles.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import './LastArticles.css'
-import SectionHeader from '../SectionHeader/SectionHeader'
+import React from "react";
+import ArticleBox from "../ArticleBox/ArticleBox";
+import SectionHeader from "./../SectionHeader/SectionHeader";
+
+import "./LastArticles.css";
+
 export default function LastArticles() {
-    return (
-        <>
-            <div className="articles">
-                <div className="container">
-                    <SectionHeader className='' title='لیست مقالات' desc='دروه های محبوب بر اساس امتیاز کاربران' />
+  return (
+    <section class="articles">
+      <div class="container">
+        <SectionHeader
+          title="جدیدترین مقاله ها"
+          desc="پیش به سوی ارتقای دانش"
+          btnTitle="تمامی مقاله ها"
+        />
 
-                </div>
-            </div>
-
-
-        </>
-    )
+        <div class="articles__content">
+          <div class="row">
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
