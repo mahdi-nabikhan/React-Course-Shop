8614dd1c80e78fc6c04c1e1b61dcc0f7bcb60ff2
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-07 11:27:44 +0330

    add Landing component and use it on Header componenet add TypeWiter for h1 text in landing

--- frontend/course-app/src/Components/Header/Header.jsx
+++ frontend/course-app/src/Components/Header/Header.jsx
@@ -3,12 +3,14 @@ import Navbar from "../Navbar/Navbar";
 import Topbar from "../Topbar/Topbar";
 
 import "./Header.css";
+import Landing from "../Landing/Landing";
 
 export default function Header() {
   return (
     <header className="header">
         <Topbar />
         <Navbar />
+        <Landing/>
     </header>
   );
 }
