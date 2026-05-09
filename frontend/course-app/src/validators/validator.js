b1947438f1d87fae879809d714a4359009a07054
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-09 10:22:21 +0330

    ** validators.js and login.jsx Refactored because a bug had to be fixed (The bug was that validation wasn't working properly and my input boxes weren't changing color after the condition was met)

--- frontend/course-app/src/validators/validator.js
+++ frontend/course-app/src/validators/validator.js
@@ -1,28 +1,29 @@
-import rules from "./rules"
-const validator = (value,validations)=>{
-    console.log(value,validations)
-    let validationsResults =[]
-    for (const validator of validations){
-        if (validator.value === rules.requiredValue){
-            value.trim().length ===0 && validationsResults.push(false)
+import rules from "./rules";
 
-        }
-        if (validator.value === rules.minValue){
-            value.trim.lenght < validator.min && validationsResults.push(false)
-        }
+const validator = (value, validations) => {
 
-        if (validator.value === rules.maxValue){
-            value.trim().lenght > validator.max && validationsResults.push(false)
-        }
-        if (validator.value === rules.emailValue){
-            value.trim().includes ("@") && validationsResults.push(false)
-        }
+  let validationResults = [];
+
+  for (const validator of validations) {
+    if (validator.value === rules.requiredValue) {
+      value.trim().length === 0 && validationResults.push(false);
     }
-    if (validationsResults.length){
-        return true
-    }else{
-        return false
+    if (validator.value === rules.minValue) {
+      value.trim().length < validator.min && validationResults.push(false);
     }
-}
-export default validator
+    if (validator.value === rules.maxValue) {
+      value.trim().length > validator.max && validationResults.push(false);
+    }
+    if (validator.value === rules.emailValue) {
+      !value.trim().includes("@") && validationResults.push(false);
+    }
+  }
 
+  if (validationResults.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export default validator;
