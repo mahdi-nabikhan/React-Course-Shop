47b5695d30a0f06e55a4c16a93c02a6209dbb1d3
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-12 10:05:34 +0330

    ** Refactoring category.jsx change and add options for sorting course with  course category

--- frontend/course-app/src/Pages/Category/Category.jsx
+++ frontend/course-app/src/Pages/Category/Category.jsx
@@ -12,12 +12,56 @@ export default function Category() {
   const [courses,setCourses]=useState([])
   const {categoryName}=useParams()
   const [shownCourses,setShownCourses] = useState([])
+  const [status,setStatus] = useState('default')
+  const[orderedCourses,setOrderedCourses]=useState([])
+  const[statusTitle,setStatusTitle] =useState('مرتب سازی پیشفرض')
   useEffect(()=>{
       fetch(`http://localhost:5000/v1/courses/category/${categoryName}`).then(res => res.json()).then(allCourses =>{
         console.log(allCourses)
         setCourses(allCourses)
       },[categoryName])
   })
+  useEffect(()=>{
+    switch(status){
+      case 'free':{
+        const freecourses = courses.filter(courses.price ===0)
+        setOrderedCourses(freecourses)
+        break;
+      }
+
+      case ' money':{
+        const notfreecourses = courses.filter(courses.price !==0)
+        setOrderedCourses(notfreecourses)
+        break;
+
+      }
+
+      case 'last':{
+        
+        setOrderedCourses(courses)
+        break;
+
+      }
+      case 'first':{
+        const reversedCourse = courses.slice().reverse()
+        setOrderedCourses(reversedCourse)
+        break;
+      }
+      case '':{
+
+      }
+
+      default: {
+          setOrderedCourses(courses)
+        }
+      
+      
+      }
+        
+  },[status])
+  const statusTitleChangeHandler = (event)=>{
+    setStatusTitle(event.target.value)
+  }
   return (
     <>
       <Topbar />
@@ -41,21 +85,31 @@ export default function Category() {
                 </span>
                 <ul class="courses-top-bar__selection-list">
                   <li class="courses-top-bar__selection-item courses-top-bar__selection-item--active">
-                    مرتب سازی پیش فرض
+                       {statusTitle}
                   </li>
-                  <li class="courses-top-bar__selection-item">
-                    مربت سازی بر اساس محبوبیت
+                  <li class="courses-top-bar__selection-item" onClick={(event)=>{setStatus('free')
+                statusTitleChangeHandler(event)
+                }}>
+                    مربت سازی بر اساس رایگان
                   </li>
-                  <li class="courses-top-bar__selection-item">
-                    مربت سازی بر اساس امتیاز
+                  <li class="courses-top-bar__selection-item" onClick={(event)=>{setStatus('money')
+                statusTitleChangeHandler(event)
+                }}>
+                    مربت سازی بر اساس پولی
                   </li>
-                  <li class="courses-top-bar__selection-item">
+                  <li class="courses-top-bar__selection-item" onClick={(event)=>{setStatus('last')
+                statusTitleChangeHandler(event)
+                }}>
                     مربت سازی بر اساس آخرین
                   </li>
-                  <li class="courses-top-bar__selection-item">
+                  <li class="courses-top-bar__selection-item" onClick={(event)=>{setStatus('cheap')
+                statusTitleChangeHandler(event)
+                }}>
                     مربت سازی بر اساس ارزان ترین
                   </li>
-                  <li class="courses-top-bar__selection-item">
+                  <li class="courses-top-bar__selection-item" onClick={(event)=>{setStatus('expensive')
+                statusTitleChangeHandler(event)
+                }}>
                     مربت سازی بر اساس گران ترین
                   </li>
                 </ul>
@@ -82,7 +136,7 @@ export default function Category() {
                     {shownCourses.map(course =>(
                       <CourseBox {...course} />
                   ))}
-                  <Pagination items={courses} itemCount={1} pathName={`category-info/${categoryName}`} setShownCourses={setShownCourses} />
+                  <Pagination items={orderedCourses} itemCount={1} pathName={`category-info/${categoryName}`} setShownCourses={setShownCourses} />
                     
                     </>)
                 }
