9e4ae9578a1bf83e5e7f02fb539113c591aad94c
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-13 08:02:39 +0330

    ++ add logout for admins in sidbar.jsx

--- frontend/course-app/src/Components/AdminPanel/Sidebar/Sidebar.jsx
+++ frontend/course-app/src/Components/AdminPanel/Sidebar/Sidebar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import './Sidebar.css'
-
+import AuthContext from "../../context/authContext";
+import swal from 'sweetalert'
+import useNavigate from 'react-router-dom'
 export default function Sidebar() {
+  const authContext=useContext(AuthContext)
+  const navigate=useNavigate()
+  const logoutAdmin = (event)=>{
+    event.prevenDefalut()
+    swal({
+      title:'با موفقیت خارج شدید',
+      icon:'success',
+      buttons : 'اوکی'
+
+    }).then(()=>{
+      authContext.logout()
+      navigate('/')
+    })
+
+  }
+  
   return (
     <div id="sidebar" class="col-2">
       <div class="sidebar-header">
@@ -50,6 +68,11 @@ export default function Sidebar() {
           <li>
             <a href="#">
               <span>دسته‌بندی‌ها</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={logoutAdmin}>
+              <span>خروج</span>
             </a>
           </li>
         </ul>
