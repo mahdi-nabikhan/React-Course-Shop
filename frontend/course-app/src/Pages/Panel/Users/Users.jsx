b1fb444fe00f4d8bde0514cc69ea050799678762
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-13 10:49:45 +0330

    ** refactor Users.jsx loading users data from api and show on table in users.jsx

--- frontend/course-app/src/Pages/Panel/Users/Users.jsx
+++ frontend/course-app/src/Pages/Panel/Users/Users.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 export default function Users() {
+  const localStorageData = localStorage.getItem('users')
+  const [allUsers,setAllUsers]=useState([]) 
+  useEffect(()=>{
+
+    fetch(`http://localhost:5000/v1/users`,{
+      headers :{
+        'Authoraztion':`Bearer ${localStorageData.token}`
+      }
+    }).then(res => res.json()).then((allUserData)=>{console.log(setAllUsers(allUserData))})
+  },[])
+  
   return (
     <>
     <DataTable title='کاربران'>
@@ -9,21 +20,22 @@ export default function Users() {
             <tr>
               <th>شناسه</th>
               <th>نام</th>
-              <th>نام خانوادگی</th>
               <th>شماره</th>
               <th>ایمیل</th>
-              <th>رمز عبور</th>
               <th>ویرایش</th>
               <th>حذف</th>
+              <th>بن</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>34223</td>
-              <td>علی</td>
-              <td>سعیدی</td>
+            {
+            
+            allUsers.map((user,index)=>{
+              <tr>
+              <td>{index +1 }</td>
+              <td>{user.name}</td>
               <td>09123443243</td>
-              <td>ali@gmail.com</td>
+              <td>{user.email}</td>
               <td>ehsan1323</td>
               <td>
                 <button type="button" class="btn btn-primary edit-btn">
@@ -36,6 +48,10 @@ export default function Users() {
                 </button>
               </td>
             </tr>
+
+            })
+            }
+            
           </tbody>
         </table>
 
