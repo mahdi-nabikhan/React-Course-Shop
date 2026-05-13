725d3a1684d1403f051d91f11c6fd61c5eebd004
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-13 11:47:01 +0330

    +add logic add temps for add a new users in panel admin

--- frontend/course-app/src/Pages/Panel/Users/Users.jsx
+++ frontend/course-app/src/Pages/Panel/Users/Users.jsx
@@ -1,7 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
 import swal from 'sweetalert'
+import {} from '../../../validators/rules'
+import { useForm } from '../../../hooks/useForm'
 export default function Users() {
+  const [formState, onInputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      username: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      phone: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
   const localStorageData = localStorage.getItem('users')
   const [allUsers,setAllUsers]=useState([]) 
   function getAllUsers(){
@@ -50,6 +77,116 @@ export default function Users() {
   }
   return (
     <>
+    <div class="home-content-edit">
+        <div class="back-btn">
+          <i class="fas fa-arrow-right"></i>
+        </div>
+        <form class="form">
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">نام و نام خانوادگی</label>
+              <Input
+                type="text"
+                className=""
+                id="name"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام و نام خانوادگی کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="family input">
+              <label class="input-title">نام کاربری</label>
+              <Input
+                type="text"
+                className=""
+                id="username"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا نام کاربری را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="email input">
+              <label class="input-title">ایمیل</label>
+              <Input
+                type="text"
+                className=""
+                id="email"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                  emailValidator(),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا ایمیل کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="password input">
+              <label class="input-title">رمز عبور</label>
+              <Input
+                type="text"
+                className=""
+                id="password"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا رمز عبور کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="phone input">
+              <label class="input-title">شماره تلفن</label>
+              <Input
+                type="text"
+                className=""
+                id="phone"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                ]}
+                onInputHandler={onInputHandler}
+                placeholder="لطفا شماره تلفن کاربر را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="bottom-form">
+              <div class="submit-btn">
+                <input type="submit" value="افزودن" onClick={registerNewUser} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     <DataTable title='کاربران'>
     <table class="table">
           <thead>
