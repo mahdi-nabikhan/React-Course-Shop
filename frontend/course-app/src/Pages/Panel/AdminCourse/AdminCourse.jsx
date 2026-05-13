e9174a44527047cb62010b88d75cc596cb76f56d
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-13 12:34:48 +0330

    ++ adding showing all courses  in panel admin

--- frontend/course-app/src/Pages/Panel/AdminCourse/AdminCourse.jsx
+++ frontend/course-app/src/Pages/Panel/AdminCourse/AdminCourse.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../../Components/AdminPanel/DataTable'
 
 export default function AdminCourse() {
+  const [courses,setCourses]=useState([])
+  useEffect(()=>{
+    const localStorageData = localStorage.getItem("user");
+    fetch("http://localhost:5000/v1/courses", {
+      headers: {
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((allCourses) => {
+        console.log(allCourses);
+        setCourses(allCourses)
+      });
+
+  },[])
   return (
-    <div>AdminCourse</div>
+    <>
+    <DataTable title='دوره ها ' >
+    <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>مبلغ</th>
+              <th>وضعیت</th>
+              <th>لینک</th>
+              <th>مدرس</th>
+              <th>دسته بندی</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {courses.map((course, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{course.name}</td>
+                <td>{course.price === 0 ? 'رایگان' : course.price.toLocaleString()}</td>
+                <td>{course.isComplete === 0 ? 'در حال برگزاری' : 'تکمیل شده'}</td>
+                <td>{course.shortName}</td>
+                <td>{course.creator}</td>
+                <td>{course.categoryID}</td>
+                <td>
+                  <button type="button" class="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger delete-btn">
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+
+
+    </DataTable>
+    </>
+
   )
 }
