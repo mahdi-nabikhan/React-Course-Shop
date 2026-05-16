d2d446b2f1b922c38cd25c7ba186e7bdd9e21948
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-16 15:04:15 +0330

    ++ adding logic for create new article and send data to api for creating a new article

--- frontend/course-app/src/Pages/Panel/AdminArticles/AdminArticles.jsx
+++ frontend/course-app/src/Pages/Panel/AdminArticles/AdminArticles.jsx
@@ -1,77 +1,116 @@
-
-import React, { useEffect, useState } from 'react'
-import DataTable from './../../../Components/AdminPanel/DataTable/DataTable'
-import {swal} from 'sweetalert'
+import React, { useEffect, useState } from "react";
+import DataTable from "./../../../Components/AdminPanel/DataTable/DataTable";
+import swal from "sweetalert";
 import { useForm } from "./../../../hooks/useForm";
+import Input from "./../../../Components/Form/Input";
+import { minValidator } from "./../../../validators/rules";
+import Editor from "../../../Components/Form/Editor";
+
 export default function Articles() {
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [articleCategory, setArticleCategory] = useState("-1");
+  const [articleCover, setArticleCover] = useState({});
+  const [articleBody, setArticleBody] = useState("");
 
-  const [articles, setArticles] = useState([])
-  function getAllArticle (){
-    fetch('http://localhost:5000/v1/articles')
-      .then(res => res.json())
-      .then(allArticles => {
-        console.log(allArticles);
-        setArticles(allArticles)
-      })
-      const [formState, onInputHandler] = useForm(
-        {
-          title: {
-            value: "",
-            isValid: false,
-          },
-          shortName: {
-            value: "",
-            isValid: false,
-          },
-          description: {
-            value: "",
-            isValid: false,
-          },
-        },
-        false
-      );
-    
-  }
-  const removeArticle = (articleID)=>{
-    const localStorageData = localStorage.getItem('user')
-    swal({
-      title:'مظمنی ',
-      icon:'warning',
-      buttons:['نه','اره']
-    }).then((res)=>{
-      if (res){
-        fetch(`http://localhost:5000/v1/article/${articleID}`,{
-          method:'DELETE',
-          headers:{
-            'Authorization':`Bearer${localStorageData.token} `
-          }
-        }).then(res=>{
-          if (res.ok){
-            swal({
-              title:'حذف شد',
-              icon:'success',
-              buttons:'ok'
-            }).then(()=>{
-              getAllArticle()
-              
-            })
-          }
-        })
-      }
-
-    })
-    
-    
-  }
+  const [formState, onInputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      shortName: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   useEffect(() => {
-    getAllArticle()
-    
-  }, [])
+    getAllArticles();
+
+    fetch(`http://localhost:5000/v1/category`)
+      .then((res) => res.json())
+      .then((allCategories) => {
+        setCategories(allCategories);
+      });
+  }, []);
+
+  function getAllArticles() {
+    fetch("http://localhost:5000/v1/articles")
+      .then((res) => res.json())
+      .then((allArticles) => {
+        console.log(allArticles);
+        setArticles(allArticles);
+      });
+  }
+
+  const removeArticle = (articleID) => {
+    const localStorageDate = JSON.parse(localStorage.getItem("user"));
+    swal({
+      title: "آیا از حذف مقاله اطمینان دارید؟`",
+      icon: "warning",
+      buttons: ["نه", "آره"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:5000/v1/articles/${articleID}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${localStorageDate.token}`,
+          },
+        }).then((res) => {
+          if (res.ok) {
+            swal({
+              title: "مقاله مورد نظر با موفقیت حذف شد",
+              icon: "success",
+              buttons: "اوکی",
+            }).then(() => {
+              getAllArticles();
+            });
+          }
+        });
+      }
+    });
+  };
+
+  const createArticle = event => {
+    event.preventDefault()
+    const localStorageDate = JSON.parse(localStorage.getItem('user'))
+    let formData = new FormData()
+    formData.append('title', formState.inputs.title.value)
+    formData.append('shortName', formState.inputs.shortName.value)
+    formData.append('description', formState.inputs.description.value)
+    formData.append('categoryID', articleCategory)
+    formData.append('cover', articleCover)
+    formData.append('body', articleBody)
+
+    fetch(`http://localhost:5000/v1/articles`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorageDate.token}`
+      },
+      body: formData
+    }).then(res => {
+      if(res.ok) {
+        swal({
+          title: 'مقاله جدید با موفقیت ایجاد شد',
+          icon: 'success',
+          buttons: 'اوکی'
+        }).then(() => {
+          getAllArticles()
+        })
+      }
+    })
+  }
 
   return (
     <>
-    <div class="container-fluid" id="home-content">
+      <div class="container-fluid" id="home-content">
         <div class="container">
           <div class="home-title">
             <span>افزودن مقاله جدید</span>
@@ -125,6 +164,18 @@ export default function Articles() {
                 <span class="error-message text-danger"></span>
               </div>
             </div>
+            <div class="col-12">
+              <div class="name input">
+                <label class="input-title" style={{ display: "block" }}>
+                  محتوا
+                </label>
+                <Editor 
+                  value={articleBody}
+                  setValue={setArticleBody}
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
             <div class="col-6">
               <div class="name input">
                 <label class="input-title" style={{ display: "block" }}>
@@ -158,16 +209,16 @@ export default function Articles() {
             <div class="col-12">
               <div class="bottom-form">
                 <div class="submit-btn">
-                  <input type="submit" value="افزودن" />
+                  <input type="submit" value="افزودن" onClick={createArticle} />
                 </div>
               </div>
             </div>
           </form>
         </div>
       </div>
-      <DataTable title="مقاله‌ها"
-      >
-         <table class="table">
+
+      <DataTable title="مقاله‌ها">
+        <table class="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -186,18 +237,15 @@ export default function Articles() {
                 <td>{article.shortName}</td>
                 <td>{article.creator.name}</td>
                 <td>
-                  <button
-                    type="button"
-                    class="btn btn-primary edit-btn"
-                  >
+                  <button type="button" class="btn btn-primary edit-btn">
                     ویرایش
                   </button>
                 </td>
                 <td>
                   <button
                     type="button"
-                    class="btn btn-danger delete-btn" 
-                    onClick={()=> removeArticle(article._id)}
+                    class="btn btn-danger delete-btn"
+                    onClick={() => removeArticle(article._id)}
                   >
                     حذف
                   </button>
@@ -208,5 +256,5 @@ export default function Articles() {
         </table>
       </DataTable>
     </>
-  )
+  );
 }
