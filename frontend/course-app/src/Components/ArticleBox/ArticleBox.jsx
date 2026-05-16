cc35663b4c5917e7b37d342fa601050b0881b3cd
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-16 15:28:44 +0330

    ** refactor articleInfo page and use dompurify ** refactor articlebox component

--- frontend/course-app/src/Components/ArticleBox/ArticleBox.jsx
+++ frontend/course-app/src/Components/ArticleBox/ArticleBox.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+
 import "./ArticleBox.css";
 
-export default function ArticleBox({ title, description, cover,shortName }) {
+export default function ArticleBox({ title, description, cover, shortName }) {
   return (
-    <div class="col-4">
-      <div class="article-card">
-        <div class="article-card__header">
-          <Link to={`/articles-info/${shortName}`} class="article-card__link-img">
+    <div className="col-4">
+      <div className="article-card">
+        <div className="article-card__header">
+          <Link
+            to={`/article-info/${shortName}`}
+            className="article-card__link-img"
+          >
             <img
-              src={cover}
-              class="article-card__img"
+              src={`http://localhost:5000/courses/covers/${cover}`}
+              className="article-card__img"
               alt="Article Cover"
             />
           </Link>
         </div>
-        <div class="article-card__content">
-          <Link to={`/articles-info/${shortName}`}  class="article-card__link">
+        <div className="article-card__content">
+          <Link
+            to={`/article-info/${shortName}`}
+            className="article-card__link"
+          >
             {title}
           </Link>
-          <p class="article-card__text">
-            {description}  
-          </p>
-          <Link to={`/articles-info/${shortName}`}  class="article-card__btn">
+          <p className="article-card__text">{description}</p>
+          <Link to={`/article-info/${shortName}`} className="article-card__btn">
             بیشتر بخوانید
           </Link>
         </div>
