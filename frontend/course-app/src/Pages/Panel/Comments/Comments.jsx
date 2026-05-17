3f5d287e474fe6cbb3200d12895b935d347cad8f
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-17 15:08:30 +0330

    Implemented the ability to add a response to each comment by sending the desired data to an API.

--- frontend/course-app/src/Pages/Panel/Comments/Comments.jsx
+++ frontend/course-app/src/Pages/Panel/Comments/Comments.jsx
@@ -1,43 +1,112 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from './../../../Components/AdminPanel/DataTable/DataTable'
+import swal from 'sweetalert'
 export default function Comments() {
-    const [comments,setComments]=useState([])
-    useEffect(()=>{
-        fetch(`http://localhost:5000/v1/comments`).then(res.josn()).then(result =>{setComments(result )})
-    },[])
-    const localStorageData = localStorage.getItem('users')
-    const removecomment = (commentID)=>{
-        swal({
-            title:'ایا از حذف مطمئن هستید',
-            icon :'warning',
-            buttons:['اره','نه']
-          }).then(result =>{
-            if (result){
-              fetch(`http://localhost:5000/auth/comments/${commentID}`,{
-                method:'DELETE',
-                headers :{
-                  'Authoraztion':`Bearer ${localStorageData.token}`
-                }
-              }).then(res =>{
-                if(res.ok){
-                  swal({
-                    title:'کاربر حذف شد',
-                    icon:'success',
-                    buttons:'ok'
-      
-                  }).then(getAllUsers)
-                }
-              })
-      
-            }
-          })
-        
+  const [comments, setComments] = useState([])
 
+  const getAllComment =()=>{
+    fetch(`http://localhost:5000/v1/comments`).then(res.josn()).then(result => { setComments(result) })
+  }
+  useEffect(() => {
+    getAllComment()
+  }, [])
+  const localStorageData = localStorage.getItem('users')
+  const removecomment = (commentID) => {
+    swal({
+      title: 'ایا از حذف مطمئن هستید',
+      icon: 'warning',
+      buttons: ['اره', 'نه']
+    }).then(result => {
+      if (result) {
+        fetch(`http://localhost:5000/auth/comments/${commentID}`, {
+          method: 'DELETE',
+          headers: {
+            'Authoraztion': `Bearer ${localStorageData.token}`
+          }
+        }).then(res => {
+          if (res.ok) {
+            swal({
+              title: 'کاربر حذف شد',
+              icon: 'success',
+              buttons: 'ok'
+
+            }).then(getAllUsers)
+          }
+        })
+
+      }
+    })
+
+
+  }
+  const showCommentBody = (commentBody) => {
+    swal({
+      title: commentBody,
+      buttons: 'ok'
+    })
+
+  }
+  const banUser = (userID) => {
+    swal({
+      title: 'مطمنی ',
+      icon: 'warning',
+      buttons: ['نه', 'اره']
+    }).then(result => {
+      if (result) {
+        fetch(`http://localhost:5000/v1/users/ban/${userID}`, {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${localStorageData.token}`
+          }
+        }).then(res => {
+          if (res.ok) {
+            swal({
+              title: 'بن شد',
+              icon: 'success',
+              buttons: 'ok'
+            })
+          }
+        }).then(()=>{
+          getAllComment()
+        })
+      }
+    })
+
+  }
+  const answerToComment = (commentID) =>{
+    swal({
+      title:'پاسخ را بنویس',
+      content :'input',
+      buttons:'ok'
+
+    }).then(answerText =>{
+      if (answerText){
+        const answerBody ={
+          body:answerText
+        }
+      fetch(`http://localhost:5000/v1/comments/answer/${commentID}`,{
+        method:'POST',
+        headers :{
+          'Content-Type':'applications/json',
+          Authorization: `Bearer ${localStorageData.token}`
+        },
+        body:JSON.stringify(answerBody)
+      }).then(res =>{
+        if (res.ok){
+          swal({
+            title:'ثبت شد',
+            icon:'success',
+          buttons:'ok'          })
+        }
+      })
     }
+
+    })
+  }
   return (
     <>
-    <DataTable title='کامنت ها'>
-    <table class="table">
+      <DataTable title='کامنت ها'>
+        <table class="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -52,42 +121,50 @@ export default function Comments() {
           </thead>
           <tbody>
             {
-            
-            comments.map((comment,index)=>{
-              <tr>
-              <td>{index +1 }</td>
-              <td>{comment.creator.name}</td>
-              <td>{comment.course}</td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  مشاهده متن
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  ثبت پاسخ
-                </button>
-              </td>
-              <td>ehsan1323</td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  ویرایش
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-danger delete-btn" onClick={removecomment(comment._id)}>
-                  حذف
-                </button>
-              </td>
-            </tr>
 
-            })
+              comments.map((comment, index) => {
+                <tr>
+                  <td className={comment.answer === 1 ?'answer-comment':'no-answer-comment'}>
+
+                    {index + 1}
+                    </td>
+                  <td>{comment.creator.name}</td>
+                  <td>{comment.course}</td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn" onClick={() => { showCommentBody(comment.body) }}>
+                      مشاهده متن
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn" onCanPlay={()=> answerToComment(comment._id)}>
+                      ثبت پاسخ
+                    </button>
+                  </td>
+                  <td>ehsan1323</td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn">
+                      ویرایش
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-danger delete-btn" onClick={removecomment(comment._id)}>
+                      حذف
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-danger delete-btn" onClick={banUser(comment.creator._id)}>
+                      بن
+                    </button>
+                  </td>
+                </tr>
+
+              })
             }
-            
+
           </tbody>
         </table>
 
-    </DataTable>
+      </DataTable>
     </>
   )
 }
