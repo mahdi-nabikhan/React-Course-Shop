2d80c642f3eedae2d2559a863938c385c532ecaf
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-17 10:56:24 +0330

    show all menus in pAdmin (change in Menus.jsx)

--- frontend/course-app/src/Pages/Panel/Menu/Menu.jsx
+++ frontend/course-app/src/Pages/Panel/Menu/Menu.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import DataTable from './../../../Components/AdminPanel/DataTable/DataTable'
 export default function Menu() {
+  const [menus,setMenus]=useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:5000/v1/menus/all`).then(res =>res.json()).then(allmenus => setMenus(allmenus))
+
+  },[])
   return (
-    <div>Menu</div>
+    <>
+     <DataTable title="منوها">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>مقصد</th>
+              <th>فرزند ...</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menus.map((menu, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{menu.title}</td>
+                <td>{menu.href}</td>
+                <td>{menu.parent ? menu.parent.title : (<i className="fa fa-check"></i>)}</td>
+                <td>
+                  <button type="button" class="btn btn-primary edit-btn">
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger delete-btn">
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DataTable>
+    
+    </>
   )
 }
